ebe32e3c0fce44072f22bd14c762ed01c26b8a4d
jordanrob709 <jordanrob709@gmail.com>
2020-11-20 18:20:26 +0300

    added about page url & view

--- pages/urls.py
+++ pages/urls.py
@@ -7,4 +7,6 @@ from .views import (
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
 ]
