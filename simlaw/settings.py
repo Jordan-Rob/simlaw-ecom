32cd3a0dbc8e338322134eff92562eabfbaafbc5
jordanrob709 <jordanrob709@gmail.com>
2020-11-26 17:38:59 +0300

    added image upload configurations

--- simlaw/settings.py
+++ simlaw/settings.py
@@ -122,8 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+#static file confi
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+# media configurations
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- simlaw/urls.py
+++ simlaw/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     path('users/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
     path('shop/', include('store.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
