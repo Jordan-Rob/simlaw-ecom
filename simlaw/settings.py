caf3c12542f961b2028dd78e6627d771b4051824
jordanrob709 <jordanrob709@gmail.com>
2020-11-27 13:01:06 +0300

    added authentication and login page

--- simlaw/settings.py
+++ simlaw/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -14,3 +17,20 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = CustomUser
         fields = ('username', 'email', 'full_names', 'phone', 'address')
+
+class MyAuthForm(ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-8'
+        self.helper.layout = Layout(
+            'email',
+            'password',
+           # StrictButton('Sign in', css_class='btn-default'),)
+)
