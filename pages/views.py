425bf7bc912009e6d42cf450078c35218facef24
jordanrob709 <jordanrob709@gmail.com>
2020-12-01 10:49:54 +0300

    added product variation pages

--- pages/views.py
+++ pages/views.py
@@ -11,3 +11,33 @@ class AboutPageView(TemplateView):
 
 class ContactPageView(TemplateView):
     template_name = 'pages/contact.html'
+
+class MaizePageView(TemplateView):
+    template_name = 'pages/maize.html'
+
+class OnionsPageView(TemplateView):
+    template_name = 'pages/onions.html'
+
+class CabbaPageView(TemplateView):
+    template_name = 'pages/cabb.html'
+
+class TomatoesPageView(TemplateView):
+    template_name = 'pages/tomatoes.html'
+
+class IndvePageView(TemplateView):
+    template_name = 'pages/indve.html'
+
+class BusbeansPageView(TemplateView):
+    template_name = 'pages/bushbeans.html'
+
+class ClimbbeansPageView(TemplateView):
+    template_name = 'pages/climbbeans.html'
+
+class GrassPageView(TemplateView):
+    template_name = 'pages/grass.html'
+
+class LegumesPageView(TemplateView):
+    template_name = 'pages/legumes.html'
+
+class WatermelonPageView(TemplateView):
+    template_name = 'pages/watermelon.html'
