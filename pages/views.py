5dda29169122223a65e0be4493b2359e759b5187
jordanrob709 <jordanrob709@gmail.com>
2020-12-02 16:41:08 +0300

    added contact form functionalities

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, View
+from .forms import ContactForm
+from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,9 +12,32 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
 
-class ContactPageView(TemplateView):
-    template_name = 'pages/contact.html'
+class ContactPageView(View):
+    def get(self, *args, **kwargs):
+        form = ContactForm
+        context = {
+            'form':form,        
+        }
+        return render(self.request, 'pages/contact.html', context )
 
+    def post(self, *args, **kwargs):
+        form = ContactForm(self.request.POST or None)
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            email = form.cleaned_data.get('email')
+            phone = form.cleaned_data.get('phone')
+            subject = form.cleaned_data.get('subject')
+            message = form.cleaned_data.get('message')
+            contact = Contact(
+                name = name,
+                email = email, 
+                phone = phone,
+                subject = subject,
+                message = message
+            )
+            contact.save() 
+            messages.success(self.request, "Your  Message was succesfully sent")
+            return redirect('home')
 class MaizePageView(TemplateView):
     template_name = 'pages/maize.html'
 
